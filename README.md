3ae41a45a7a332bc837f3f13749f4904f676096a
Michael Wittig <post@michaelwittig.info>
2016-03-23 08:30:36 +0100

    using new amazon linux

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # S3 VirusScan
 
+Antivirus for S3 buckets.
+
+## Features
+
+* Uses ClamAV to scan your files on S3
+* Automatically updates ClamAV db every 2 hours
+* Scale from 1 or 0 to $N EC2 instances to distribute workload
+
 ## Installation
 
 create stack based on `template.json`
@@ -10,5 +18,6 @@ connect Create Events of the S3 buckets to be scanned with the SQS queue from th
 
 ## TODO
 
-* deploy worker.rb in UserData
-* update ClamAV virus definitions
+* What about source from github (https://github.com/<your directory>/(zipball|tarball)/<version> )
+* Auto Scaling based on Queue Size
+* What about S3 versioned buckets
--- worker.rb
+++ worker.rb
@@ -3,30 +3,33 @@
 require 'aws-sdk'
 require 'json'
 require 'uri'
+require 'yaml'
 
-Aws.config.update(region: 'us-east-1')
+conf = YAML::load_file('s3-virusscan.conf')
+
+Aws.config.update(region: conf['region'])
 s3 = Aws::S3::Client.new()
-poller = Aws::SQS::QueuePoller.new('https://sqs.us-east-1.amazonaws.com/878533158213/s3virusscan-ScanQueue-RLW6UM890D65')
+poller = Aws::SQS::QueuePoller.new(conf['queue'])
 
 poller.poll do |msg|
   body = JSON.parse(msg.body)
-  body["Records"].each do |record|
-    key = URI.decode(record["s3"]["object"]["key"]).gsub("+", " ")
+  body['Records'].each do |record|
+    key = URI.decode(record['s3']['object']['key']).gsub('+', ' ')
     s3.get_object(
       response_target: '/tmp/target',
-      bucket: record["s3"]["bucket"]["name"],
+      bucket: record['s3']['bucket']['name'],
       key: key
     )
-    if system("clamscan /tmp/target")
+    if system('clamscan /tmp/target')
       puts "file #{key} is save"
     else
       puts "file #{key} is infected, deleting"
       s3.delete_object(
-        bucket: record["s3"]["bucket"]["name"],
+        bucket: record['s3']['bucket']['name'],
         key: key
       )
       puts "file #{key} was deleted"
     end
-    system("rm /tmp/target")
+    system('rm /tmp/target')
   end
 end
