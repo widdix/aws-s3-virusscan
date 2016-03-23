4371da54fae01a8c1eb151d36d1154f3822233ba
Michael Wittig <post@michaelwittig.info>
2016-03-23 10:04:48 +0100

    fix

--- worker.rb
+++ worker.rb
@@ -4,31 +4,36 @@ require 'aws-sdk'
 require 'json'
 require 'uri'
 require 'yaml'
+require 'syslog/logger'
 
-conf = YAML::load_file('s3-virusscan.conf')
+log = Syslog::Logger.new 's3-virusscan'
+conf = YAML::load_file(__dir__ + '/s3-virusscan.conf')
 
 Aws.config.update(region: conf['region'])
 s3 = Aws::S3::Client.new()
 poller = Aws::SQS::QueuePoller.new(conf['queue'])
 
+log.info "s3-virusscan started"
+
 poller.poll do |msg|
   body = JSON.parse(msg.body)
   body['Records'].each do |record|
     key = URI.decode(record['s3']['object']['key']).gsub('+', ' ')
+    log.debug "scanning #{key}..."
     s3.get_object(
       response_target: '/tmp/target',
       bucket: record['s3']['bucket']['name'],
       key: key
     )
     if system('clamscan /tmp/target')
-      puts "file #{key} is save"
+      log.debug "#{key} was scanned without findings"
     else
-      puts "file #{key} is infected, deleting"
+      log.error "#{key} is infected, deleting..."
       s3.delete_object(
         bucket: record['s3']['bucket']['name'],
         key: key
       )
-      puts "file #{key} was deleted"
+      log.error "#{key} was deleted"
     end
     system('rm /tmp/target')
   end
