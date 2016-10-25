f5ae57d31d214e5026c915ec77cb1daaac1c1b61
Chris Clarke <cc@talis.com>
2016-10-25 15:41:07 +0100

    Converting to class for DRY, changing conf variable name to be more ruby-esq

--- worker.rb
+++ worker.rb
@@ -6,77 +6,93 @@ require 'uri'
 require 'yaml'
 require 'syslog/logger'
 
-log = Syslog::Logger.new 's3-virusscan'
-conf = YAML::load_file(__dir__ + '/s3-virusscan.conf')
 
-Aws.config.update(region: conf['region'])
-s3 = Aws::S3::Client.new()
+class ClamScanWorker
 
-poller = Aws::SQS::QueuePoller.new(conf['queue'])
+  attr_reader :log, :conf, :sns
 
-clean_status = 'clean';
-infected_status = 'infected';
+  CLEAN_STATUS = 'clean'
+  INFECTED_STATUS = 'infected'
 
-log.info "s3-virusscan started"
+  def initialize
+    @log = Syslog::Logger.new 's3-virusscan'
+    @conf = YAML::load_file(__dir__ + '/s3-virusscan.conf')
+    Aws.config.update(region: conf['region'])
+    @sns = Aws::SNS::Client.new()
+  end
 
-def publish_notification(msg,status)
-  log.info msg
-  sns = Aws::SNS::Client.new()
-  sns.publish(
-    topic_arn: conf['topic'],
-    message: msg,
-    subject: "s3-virusscan s3://#{bucket}",
-    message_attributes: {
-      "key" => {
-        data_type: "String",
-        string_value: "s3://#{bucket}/#{key}"
-      },
-      "status" => {
-        data_type: "String",
-        string_value: status
-      }
-    }
-  )
-end
+  def perform
+    log.info "s3-virusscan started"
 
-poller.poll do |msg|
-  body = JSON.parse(msg.body)
-  if body.key?('Records')
-    body['Records'].each do |record|
-      bucket = record['s3']['bucket']['name']
-      key = URI.decode(record['s3']['object']['key']).gsub('+', ' ')
-      log.debug "scanning s3://#{bucket}/#{key}..."
-      begin
-        s3.get_object(
-          response_target: '/tmp/target',
-          bucket: bucket,
-          key: key
-        )
-      rescue Aws::S3::Errors::NoSuchKey
-        log.error "s3://#{bucket}/#{key} does no longer exist"
-        next
-      end
-      if system('clamscan /tmp/target')
-        if conf['reportClean']
-          publish_notification("s3://#{bucket}/#{key} is clean",clean_status);
-        else
-          # log only, no notification
-          log.info "s3://#{bucket}/#{key} is clean"
-        end
-      else
-        if conf['delete']
-          publish_notification("s3://#{bucket}/#{key} is infected, deleting...",infected_status);
-          s3.delete_object(
-            bucket: bucket,
-            key: key
-          )
-          log.info "s3://#{bucket}/#{key} was deleted"
-        else
-          publish_notification("s3://#{bucket}/#{key} is infected",infected_status);
+    s3 = Aws::S3::Client.new()
+    poller = Aws::SQS::QueuePoller.new(conf['queue'])
+
+    poller.poll do |msg|
+      body = JSON.parse(msg.body)
+      if body.key?('Records')
+        body['Records'].each do |record|
+          bucket = record['s3']['bucket']['name']
+          key = URI.decode(record['s3']['object']['key']).gsub('+', ' ')
+          log.debug "scanning s3://#{bucket}/#{key}..."
+          begin
+            s3.get_object(
+              response_target: '/tmp/target',
+              bucket: bucket,
+              key: key
+            )
+          rescue Aws::S3::Errors::NoSuchKey
+            log.error "s3://#{bucket}/#{key} does no longer exist"
+            next
+          end
+          if system('clamscan /tmp/target')
+            if conf['report_clean']
+              publish_notification(bucket,key,CLEAN_STATUS);
+            else
+              # log only, no notification
+              log.info "s3://#{bucket}/#{key} is clean"
+            end
+          else
+            publish_notification(bucket,key,INFECTED_STATUS);
+            if conf['delete']
+              s3.delete_object(
+                bucket: bucket,
+                key: key
+              )
+              log.info "s3://#{bucket}/#{key} was deleted"
+            end
+          end
+          system('rm /tmp/target')
         end
       end
-      system('rm /tmp/target')
     end
   end
+
+  private
+
+  def publish_notification(bucket,key,status)
+    msg = "s3://#{bucket}/#{key} is #{status}"
+    if conf['delete']
+      msg << ", deleting..."
+    end
+    log.info msg
+    sns.publish(
+      topic_arn: conf['topic'],
+      message: msg,
+      subject: "s3-virusscan s3://#{bucket}",
+      message_attributes: {
+        "key" => {
+          data_type: "String",
+          string_value: key
+        },
+        "status" => {
+          data_type: "String",
+          string_value: status
+        }
+      }
+    )
+  end
+
 end
 
+# do some work
+ClamScanWorker.new.perform
